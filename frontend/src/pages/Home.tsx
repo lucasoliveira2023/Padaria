3de376f2d76b41d6a09280b874c75d49d4221c33
lucas oliveira <blesslucas89@gmail.com>
2025-05-26 18:28:28 -0500

    feat: setup inicial do front-end com Vite e TypeScript

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Bem-vindo ao MVP React + Vite + TypeScript</h1>
+    </div>
+  );
+};
+
+export default Home;
