c7128c06ead7070f0caf28459a2ef25e3ea338b8
lucas oliveira <blesslucas89@gmail.com>
2025-05-29 18:20:55 -0500

    config incial do gihub actions

--- backend/padaria/settings/base.py
+++ backend/padaria/settings/base.py
@@ -25,12 +25,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ll!@ikx6)z4j935=#6+#i)!%+vkz#bt17eg+oz((%hv3^d#in_'
+#SECRET_KEY = 'django-insecure-ll!@ikx6)z4j935=#6+#i)!%+vkz#bt17eg+oz((%hv3^d#in_'
+SECRET_KEY = os.getenv('SECRET_KEY', 'unsafe-secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost').split(' ')
 
 
 # Application definition
@@ -84,11 +85,11 @@ WSGI_APPLICATION = 'padaria.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('POSTGRES_HOST'),
-        'PORT': os.getenv('POSTGRES_PORT'),
+        'NAME': os.getenv('POSTGRES_DB', 'padaria_db'),
+        'USER': os.getenv('POSTGRES_USER', 'padaria_user'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', '123456'),
+        'HOST': os.getenv('POSTGRES_HOST', 'db'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
 
